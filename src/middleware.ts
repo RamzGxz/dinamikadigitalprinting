78acbb628738bfbbbcb4ffa63a1789683daa6bf5
RamzGxz <ranzdaffa32@gmail.com>
2024-06-10 00:26:52 +0700

    changes SEO and View

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
 
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export default async function middleware(req: NextRequest) {
 
-  const session = await getSession();
-  console.log(session)
-  if (session?.user) {
+  const { data } = useSession()
+
+  if (data) {
     return NextResponse.next()
   } else {
     return NextResponse.redirect(new URL('/auth/login', req.url))
